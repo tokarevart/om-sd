d2f67f5628009c4591e770cc7804b8006fe09865
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-29 02:23:15 +0500

    upd

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+// pub use generic_array::{ArrayLength, GenericArray, typenum::*};
+pub use nalgebra as na;
+use na::Vector2;
+
+pub fn search(
+    init: Vector2<f64>, eps: f64, 
+    f: impl Fn(Vector2<f64>) -> f64, 
+    grad: impl Fn(Vector2<f64>) -> Vector2<f64>,
+    search_lam: impl Fn(Vector2<f64>, &dyn Fn(f64) -> f64) -> f64,
+) -> Vector2<f64> {
+    
+    assert!(eps > 0.0);
+
+    let mut x = init;
+    let mut gradx = grad(x);
+    while grad(x).norm() > eps {
+        let nextx = |lam: f64| x - gradx * lam;
+        let lam = search_lam(x, &|lam: f64| f(nextx(lam)));
+        x = nextx(lam);
+        gradx = grad(x);
+    }
+    
+    x
+}
