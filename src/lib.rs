d8fe1cdb66d8de65afafc4f5767b41ee1c579376
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-29 04:02:06 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-// pub use generic_array::{ArrayLength, GenericArray, typenum::*};
 pub use nalgebra as na;
 use na::Vector2;
 
@@ -6,7 +5,7 @@ pub fn search(
     init: Vector2<f64>, eps: f64, 
     f: impl Fn(Vector2<f64>) -> f64, 
     grad: impl Fn(Vector2<f64>) -> Vector2<f64>,
-    search_lam: impl Fn(Vector2<f64>, &dyn Fn(f64) -> f64) -> f64,
+    search_lam: impl Fn(&dyn Fn(f64) -> f64) -> f64,
 ) -> Vector2<f64> {
     
     assert!(eps > 0.0);
@@ -15,7 +14,7 @@ pub fn search(
     let mut gradx = grad(x);
     while grad(x).norm() > eps {
         let nextx = |lam: f64| x - gradx * lam;
-        let lam = search_lam(x, &|lam: f64| f(nextx(lam)));
+        let lam = search_lam(&|lam: f64| f(nextx(lam)));
         x = nextx(lam);
         gradx = grad(x);
     }
