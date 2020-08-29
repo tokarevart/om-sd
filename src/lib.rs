abaabebd850f40072918cb0cd5f7c83e72aa70a1
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-29 08:25:26 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
 pub use nalgebra as na;
-use na::Vector2;
 
-pub fn search(
-    init: Vector2<f64>, eps: f64, 
-    f: impl Fn(Vector2<f64>) -> f64, 
-    grad: impl Fn(Vector2<f64>) -> Vector2<f64>,
-    search_lam: impl Fn(&dyn Fn(f64) -> f64) -> f64,
-) -> Vector2<f64> {
-    
-    assert!(eps > 0.0);
-
-    let mut x = init;
-    let mut gradx = grad(x);
-    while grad(x).norm() > eps {
-        let nextx = |lam: f64| x - gradx * lam;
-        let lam = search_lam(&|lam: f64| f(nextx(lam)));
-        x = nextx(lam);
-        gradx = grad(x);
-    }
-    
-    x
+macro_rules! make_search_fn {
+    ($name:ident, $argtype:ty) => {
+        pub fn $name(
+            init: $argtype, eps: f64, 
+            f: impl Fn($argtype) -> f64, 
+            grad: impl Fn($argtype) -> $argtype,
+            search_lam: impl Fn(&dyn Fn(f64) -> f64) -> f64,
+        ) -> $argtype {
+            
+            assert!(eps > 0.0);
+        
+            let mut x = init;
+            let mut gradx = grad(x);
+            while grad(x).norm() > eps {
+                let nextx = |lam: f64| x - gradx * lam;
+                let lam = search_lam(&|lam: f64| f(nextx(lam)));
+                x = nextx(lam);
+                gradx = grad(x);
+            }
+            
+            x
+        }
+    };
 }
+
+make_search_fn!(search_2d, na::Vector2<f64>);
+make_search_fn!(search_3d, na::Vector3<f64>);
